1c91ae62c666baf949f24b2fbb6ac9cad2cfb6be
КYННЭЙ <burnashev@imac-kynnej.dlink>
2012-11-07 00:52:06 +1000

    Initial Commit

---
+++ Проект 1 ДНК/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Проект 1 ДНК
+//
+//  Created by КYННЭЙ on 07.11.12.
+//  Copyright (c) 2012 КYННЭЙ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
